7147590f6b46e5852b377371980dac68faaf1600
FlynnayBoi <78829192+FlynnayBoi@users.noreply.github.com>
2021-02-24 12:37:03 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,384 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    led.plot(0, 2)
+    led.plot(1, 1)
+    led.plot(1, 2)
+    led.plot(1, 3)
+    led.plot(2, 1)
+    led.plot(2, 2)
+    led.plot(2, 3)
+    led.plot(2, 4)
+    led.plot(3, 1)
+    led.plot(3, 2)
+    led.plot(3, 3)
+    led.plot(4, 2)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        # . # . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        # # # . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # . .
+        # # # # .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . # # . .
+        # . # # #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . . . . .
+        # . . # #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . . . . .
+        . . . . .
+        # . . . #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        # # # . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # . .
+        # # # # .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . # # . .
+        # . # # #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . . . . .
+        # . . # #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . . . . .
+        . . . . .
+        # . . . #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        # # # . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # . .
+        # # # # .
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . # # . .
+        # . # # #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . . . . .
+        # . . # #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . . . . .
+        . . . . .
+        # . . . #
+        # # . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        # # . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
